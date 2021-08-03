da18297824fa23922f055a67c281ed4a5ce76192
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2021-08-03 18:39:59 -0400

    changed to remove numpy commands

--- subset-sum.py
+++ subset-sum.py
@@ -1,16 +1,21 @@
-  import numpy as np
-  def dynIsSubset(nums, sum):
-  	dp = np.zeros(len(set)+1, sum+1)
-  	
-    for s in range(1, sum+1):
-        for n in range(1,len(nums)+1):
-      	if s == 0:
-        	dp[s][n] = True
-      	elif s !=0 and n==0:
-        	dp[s]][n] == False
-        else:
-      	    if set[s][n] <= s:
-        	    dp[s][n] = dp[s][n-1] or dp[s-nums[n]][n-1]
-            else:
-        	    dp[s][n] = dp[s][n]
-    return dp[sum][len(nums)]
+class Solution(object):
+      def subsetSum(self, nums, target):
+          length = len(nums)
+          memo = [[False for col in range(target+1)] for row in range(length+1)]
+          for n in range(length+1): #target = 0
+              memo[n][0] = True
+          #second base case need not be taken as initialization is all False
+
+          for n in range(1, length+1):
+              for t in range(1, target+1):
+                  if nums[n-1] <= t:
+                      memo[n][t] = memo[n-1][t - nums[n-1]] or memo[n-1][t]
+                  else:
+                      memo[n][t] = memo[n-1][t]
+          return memo[length][target]
+
+#Uncomment to test
+#nums = [1,2,3,7]
+#target = 11
+#sol = Solution()
+#print(sol.subsetSum(nums, target))
