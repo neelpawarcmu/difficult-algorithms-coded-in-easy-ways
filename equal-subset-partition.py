39bf8abbbb1337545403707c83f98b6f7a09ad88
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2021-08-03 18:43:26 -0400

    Create equal-subset-partition.py

---
+++ equal-subset-partition.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canPartition(self, nums):
+        def subsetSum(numLen, target):
+            memo = [[False for col in range(target+1)] for row in range(numLen+1)]
+            for n in range(numLen): #target = 0
+                memo[n][0] = True
+            #second base case need not be taken as initialization is all False
+            
+            for n in range(1, numLen+1):
+                for t in range(1, target+1):
+                    if nums[n-1] <= t:
+                        memo[n][t] = memo[n-1][t - nums[n-1]] or memo[n-1][t]
+                    else:
+                        memo[n][t] = memo[n-1][t]
+            return memo[numLen][target]
+        
+        totalSum = sum(nums) #O(n) time
+        n = len(nums)
+        
+        
+        if totalSum %2 == 1:
+            return False
+        else:
+            return subsetSum(n, totalSum//2)
