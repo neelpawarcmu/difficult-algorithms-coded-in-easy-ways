ccd3ab8994e43db299824e960fbc893b136e4574
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2021-08-04 19:53:49 -0400

    cut-rod solution
    recursive solution still incomplete

--- unbounded-knapsack/cut-rod.py
+++ unbounded-knapsack/cut-rod.py
@@ -1,8 +1,30 @@
-if not rodLength or not lengths:
+class Solution:
+    def memoize(self, r, l, p):
+        memo = {}
+        def helper(r, l, p):
+            if (r,l,p) not in memo:
+                memo[(r,l,p)] = self.cutRodRec(r, l, p)
+            return helper
+    
+    def cutRodRec(self, rod, lengths, prices):
+        if not rod or not lengths:
             return 0
         n = len(lengths) - 1
-        if lengths[n] <= rodLength:
-            maxPrice = max(self.cutRod(rodLength-lengths[n], lengths, prices), self.cutRod(rodLength, lengths[:n], prices[:n])
+        maxPrice = -1
+        if lengths[n] <= rod:
+            return max(self.cutRod(rod-lengths[n], lengths, prices), self.cutRod(rod, lengths[:n], prices[:n]))
         else:
-            maxPrice = self.cutRod(rodLength, lengths[:n], prices[:n])
-        return price
+            return self.cutRod(rod,lengths[:n], prices[:n])
+            
+         
+    def cutRod(self, rod, lengths, prices):
+        n = len(lengths)
+        dp = [[0 for col in range(rod+1)] for row in range(n+1)]
+        
+        for l in range(1, n+1):
+            for r in range(1, rod+1):
+                if lengths[l-1] <= r:
+                    dp[l][r] = max(prices[l-1] + dp[l-1][r-lengths[l-1]], dp[l-1][r])
+                else:
+                    dp[l][r] = dp[l-1][r]
+        return dp[n][rod]
