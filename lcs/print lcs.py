28d83eb321ee544baa4b63bf710b2386f1d61a0c
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2021-08-05 22:43:04 -0400

    Update print lcs.py

--- lcs/print lcs.py
+++ lcs/print lcs.py
@@ -20,7 +20,7 @@ class Solution(object):
                     dp[row][col] = max(dp[row-1][col], dp[row][col-1])    
         
         lcs = ''
-        t1, t2 = len(text1), len(text2)
+        t1, t2 = len(text1)-1, len(text2)-1
         while t1 and t2:
             if text1[t1-1] == text2[t2-1]:
                 lcs += text1[t1]
